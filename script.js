20ed9cb44bdd7690bfe0e5a030b055db6cfd0864
mzwang25 <mzwang25@gmail.com>
2021-05-10 21:25:04 -0700

    try

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+var video;
 var canv = new Array(5);
 var ctx = new Array(5);
 var drawing = new Array(5);
 var xy0 = null;
 var letters = [
-  ['a', 'b', 'c', 'd', 'e', 'f'],
-  ['g', 'h', 'i', 'j', 'k', 'l'],
-  ['m', 'n', 'o', 'p', 'q', 'r'],
-  ['s', 't', 'u', 'v', 'w', 'x'],
-  ['y', 'z']
+  ["a", "b", "c", "d", "e", "f"],
+  ["g", "h", "i", "j", "k", "l"],
+  ["m", "n", "o", "p", "q", "r"],
+  ["s", "t", "u", "v", "w", "x"],
+  ["y", "z"],
 ];
 var pathx = [];
 var pathy = [];
 
 var startup = () => {
+
   for (var i = 0; i < 6; i++) {
     canv[i] = document.getElementById("canv" + i);
     ctx[i] = canv[i].getContext("2d");
@@ -27,67 +29,63 @@ var startup = () => {
       drawing[i] = true;
 
       let rect = canv[i].getBoundingClientRect();
-      let x = e.clientX - rect.left, y = e.clientY - rect.top;
-      xy0 = { x: x, y: y }
-
+      let x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+      xy0 = { x: x, y: y };
     });
 
     canv[i].addEventListener("mouseup", (e) => {
-      if (!drawing[i])
-        return;
+      if (!drawing[i]) return;
 
       ctx[i].closePath();
       ctx[i].clearRect(0, 0, 80, 80);
       drawing[i] = false;
 
       let rect = canv[i].getBoundingClientRect();
-      let x = e.clientX - rect.left, y = e.clientY - rect.top;
+      let x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
 
-      if(i === 5) {
-        emojiDecide("hello")
-      }
-      else {
+      if (i === 5) {
+        emojiDecide("hello");
+      } else {
         decide(i, x - xy0.x, y - xy0.y);
       }
 
-      document.getElementById("text")
-
+      document.getElementById("text");
     });
 
     canv[i].addEventListener("mouseout", (e) => {
-      if (!drawing[i])
-        return;
+      if (!drawing[i]) return;
 
       ctx[i].closePath();
       ctx[i].clearRect(0, 0, 80, 80);
       drawing[i] = false;
 
       let rect = canv[i].getBoundingClientRect();
-      let x = e.clientX - rect.left, y = e.clientY - rect.top;
+      let x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
 
-      if(i === 5) {
+      if (i === 5) {
         pathx = [];
         pathy = [];
-      }
-      else {
+      } else {
         decide(i, x - xy0.x, y - xy0.y);
       }
-
     });
 
     canv[i].addEventListener("mousemove", (e) => {
-      if (!drawing[i])
-        return;
+      if (!drawing[i]) return;
 
       let rect = canv[i].getBoundingClientRect();
-      let x = e.clientX - rect.left, y = e.clientY - rect.top;
+      let x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
       ctx[i].lineTo(x, y);
       ctx[i].moveTo(x, y);
-      pathx.push(x)
-      pathy.push(y)
+      pathx.push(x);
+      pathy.push(y);
       ctx[i].stroke();
     });
-  }
+  };
 
   attachListeners(0);
   attachListeners(1);
@@ -95,137 +93,113 @@ var startup = () => {
   attachListeners(3);
   attachListeners(4);
   attachListeners(5);
-}
-
+};
 
 var decide = (id, dx, dy) => {
-  pathx = []
-  pathy = []
+  pathx = [];
+  pathy = [];
   if (Math.abs(dy) < 10) {
-    if (dx > 0)
-      appendText(letters[id][0]);
-    else
-      appendText(letters[id][4]);
+    if (dx > 0) appendText(letters[id][0]);
+    else appendText(letters[id][4]);
 
     return;
   }
 
   if (Math.abs(dx) < 10) {
-    if (dy > 0)
-      appendText(letters[id][5]);
-    else
-      appendText(letters[id][2]);
+    if (dy > 0) appendText(letters[id][5]);
+    else appendText(letters[id][2]);
 
     return;
   }
 
-  if (dx > 0 && dy < 0)
-    appendText(letters[id][1]);
-  else if (dx < 0 && dy < 0)
-    appendText(letters[id][3]);
-  else if (dx > 0 && dy > 0)
-    appendText(" ");
-  else
-    textdelete();
-}
+  if (dx > 0 && dy < 0) appendText(letters[id][1]);
+  else if (dx < 0 && dy < 0) appendText(letters[id][3]);
+  else if (dx > 0 && dy > 0) appendText(" ");
+  else textdelete();
+};
 
 var appendText = (char) => {
   document.getElementById("text").value += char;
-}
+};
 
 var textdelete = () => {
-  document.getElementById("text").value
-    = document.getElementById("text").value.slice(0, -1);
-}
+  document.getElementById("text").value = document
+    .getElementById("text")
+    .value.slice(0, -1);
+};
 
 var emojiDecide = (v) => {
+  var vecx = [];
+  var vecy = [];
 
-  var vecx = []
-  var vecy = []
-
-  for(var i = 5; i < pathx.length; i+=5)
-  {
-    vecx.push(pathx[i] - pathx[i-5])
-    vecy.push(pathy[i] - pathy[i-5])
+  for (var i = 5; i < pathx.length; i += 5) {
+    vecx.push(pathx[i] - pathx[i - 5]);
+    vecy.push(pathy[i] - pathy[i - 5]);
   }
 
-
-  var dtheta = []
-  for(var i = 1; i < vecx.length; i++)
-  {
-    var v0x = vecx[i-1]
-    var v1x = vecx[i]
-    var v0y = vecy[i-1]
-    var v1y = vecy[i]
+  var dtheta = [];
+  for (var i = 1; i < vecx.length; i++) {
+    var v0x = vecx[i - 1];
+    var v1x = vecx[i];
+    var v0y = vecy[i - 1];
+    var v1y = vecy[i];
 
     var dot = v0x * v1x + v0y * v1y;
-    var mag0 = Math.sqrt(v0x * v0x + v0y * v0y)
-    var mag1 = Math.sqrt(v1x * v1x + v1y * v1y)
+    var mag0 = Math.sqrt(v0x * v0x + v0y * v0y);
+    var mag1 = Math.sqrt(v1x * v1x + v1y * v1y);
 
     var theta = Math.acos(dot / (mag0 * mag1));
 
-    if(!isNaN(theta))
-      dtheta.push(theta * 180/Math.PI)
+    if (!isNaN(theta)) dtheta.push((theta * 180) / Math.PI);
   }
 
-
   var sum = 0;
-  for(var i = 0; i < dtheta.length; i++)
-  {
+  for (var i = 0; i < dtheta.length; i++) {
     sum += dtheta[i];
   }
 
-  function getStandardDeviation (array) {
-    const n = array.length
-    const mean = array.reduce((a, b) => a + b) / n
-    return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
-  }
+  var getStandardDeviation = (array) => {
+    const n = array.length;
+    const mean = array.reduce((a, b) => a + b) / n;
+    return Math.sqrt(
+      array.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n
+    );
+  };
 
-  
-  var x1 = pathx[pathx.length - 1]
-  var y1 = pathy[pathx.length - 1]
-  var x0 = pathx[0]
-  var y0 = pathy[0]
+  var x1 = pathx[pathx.length - 1];
+  var y1 = pathy[pathx.length - 1];
+  var x0 = pathx[0];
+  var y0 = pathy[0];
 
-  var d = (x1-x0)*(x1-x0) + (y1-y0)*(y1-y0);
-  var dy = 0
+  var d = (x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0);
+  var dy = 0;
 
   var ybounces = 0;
-  var dir = Math.sign(pathy[1]-pathy[0])
-  for(var i = 2; i < pathy.length; i++)
-  {
-    if(Math.sign(pathy[i] - pathy[i-1]) !== dir && Math.abs(pathy[i] - pathy[i-1]) > 0.1)
-    {
-      dir = Math.sign(pathy[i] - pathy[i-1]);
+  var dir = Math.sign(pathy[1] - pathy[0]);
+  for (var i = 2; i < pathy.length; i++) {
+    if (
+      Math.sign(pathy[i] - pathy[i - 1]) !== dir &&
+      Math.abs(pathy[i] - pathy[i - 1]) > 0.1
+    ) {
+      dir = Math.sign(pathy[i] - pathy[i - 1]);
       ybounces += 1;
     }
-
   }
 
-    console.log(getStandardDeviation(dtheta));
+  console.log(getStandardDeviation(dtheta));
 
-  if(d < 100)
-  {
-    if (getStandardDeviation(dtheta) < 25)
-    {
-      appendText(String.fromCodePoint(0x1F631));
-    }
-    else 
-      appendText(String.fromCodePoint(0x1F60D));
-  }
-  else
-  {
-    if(getStandardDeviation(dtheta) > 25)
-    {
-      appendText(String.fromCodePoint(0x1F389));
-    }
-
-    else if(pathy[3] < pathy[0])
-      appendText(String.fromCodePoint(0x1F622));
-
-    else
-      appendText(String.fromCodePoint(0x1F600));
+  if (d < 100) {
+    if (getStandardDeviation(dtheta) < 25) {
+      appendText(String.fromCodePoint(0x1f631));
+    } else appendText(String.fromCodePoint(0x1f60d));
+  } else {
+    if (getStandardDeviation(dtheta) > 25) {
+      appendText(String.fromCodePoint(0x1f389));
+    } else if (pathy[3] < pathy[0]) appendText(String.fromCodePoint(0x1f622));
+    else appendText(String.fromCodePoint(0x1f600));
   }
   pathx = [];
   pathy = [];
-}
+};
+
+
